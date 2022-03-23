efbcb6e1e50abad6a060f5e3d4cd3cf33650c1f0
Matheus0100 <matheusmouradasilva2018@gmail.com>
2022-03-22 22:22:54 -0300

    Alteração 02

--- Script1.js
+++ Script1.js
@@ -1,12 +1,12 @@
 var listadepecas = ["Amortecedor", "Motor", "Filtro de ar","Amortecedor", "Motor", "Filtro de ar","Amortecedor", "Motor",
  "Filtro de ar","Amortecedor", "Motor", "Filtro de ar"]
-let peso = 99
 
+ let peso = 101
 
 if (peso<100){
-    console.log("A peca deve pesar no mínimo 100g")
+    console.log("A peça deve pesar no mínimo 100g")
 } else {
-    console.log("A peca possui peso adequado")
+    console.log("A peça possui peso adequado")
 }
 
 if (listadepecas.length < 10){
@@ -18,7 +18,7 @@ if (listadepecas.length < 10){
 let NomePeca = "Disco de Freio"
 
 if(NomePeca.length > 3){
-    console.log("Nome da peça está adequado para o cadastro!")
+    console.log("O Nome da peça está adequado para o cadastro!")
 }else if (NomePeca.length == 0){
     console.log("O Nome da peça não pode ser vazio")
 }else {
